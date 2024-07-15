e33761477fc112dcb3364aea562bb0e17f829b3c
Olenka Vargas Lazarte <olenka@ravn.co>
2024-07-15 10:36:41 -0500

    feat: improve structure tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // src/App.tsx
 import React from "react";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Home from "./components/Home";
+import Header from "./views/components/Header";
+import Sidebar from "./views/components/Sidebar";
+import Home from "./views/layouts/Home";
 
 const App: React.FC = () => {
   return (
